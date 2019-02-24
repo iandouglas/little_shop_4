2147b1858fb8c3765d405f68a7174c4d17dca60f
plapicola <p.lapicola@gmail.com>
2019-02-24 00:10:25 -0700

    Adds tests for User#redeemed_coupon?(coupon) method

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -3,8 +3,16 @@ class CouponsController < ApplicationController
 
   def create
     coupon = Coupon.find_by_name(params[:coupon])
-    session[:coupon] = coupon.id
-    flash[:success] = "Coupon \"#{coupon.name}\" has been applied."
+    if coupon && !coupon.disabled
+      unless  current_user && current_user.redeemed_coupon?(coupon)
+        session[:coupon] = coupon.id
+        flash[:success] = "Coupon \"#{coupon.name}\" has been applied."
+      else
+        flash[:danger] = "Coupon \"#{coupon.name}\" has already been redeemed"
+      end
+    else
+      flash[:warning] = "Invalid coupon name."
+    end
     redirect_to cart_path
   end
 end
